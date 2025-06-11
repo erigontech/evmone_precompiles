3fecba774bbd75f3848c7e88af690c50abe98888
yperbasis <andrey.ashikhmin@gmail.com>
2025-06-11 06:22:44 +0200

    add comment

--- capi.h
+++ capi.h
@@ -5,6 +5,15 @@
 extern "C" {
 #endif
 
+// evmone_capi_ec_add performs point addition on the elliptic curve alt_bn128.
+// See EIP-196: Precompiled contracts for addition and scalar multiplication on the elliptic curve alt_bn128.
+//
+// Returns: 1: addition was successful
+//          0: addition was not successful (an input point was invalid)
+// Args:
+//  Out:    out:  the serialized 64-byte resulting curve point (cannot be NULL)
+//  In:     p_in: pointer to the 1st serialized 64-byte curve point (cannot be NULL)
+//          q_in: pointer to the 2nd serialized 64-byte curve point (cannot be NULL)
 int evmone_capi_ec_add(
     unsigned char* out,
     const unsigned char* p_in,
